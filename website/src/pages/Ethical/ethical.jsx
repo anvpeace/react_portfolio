5ca6c4e1807779897ab524bee6b46faf1812c1e0
anvpeace <cruzpeacem@gmail.com>
2025-06-29 09:53:40 -0400

    updates

--- website/src/pages/Ethical/ethical.jsx
+++ website/src/pages/Ethical/ethical.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import Nav from "../../components/Navbar/nav";
 import Footer from "../../components/Footer/footer";
-import Intro from "../../components/Intro/intro";
-import Card from "../../components/Card/card";
 
 
 function Ethical (){
     return <>
     <Nav/>
         <h1> Ethical working on click</h1>
-        <Intro/>
-        <div>
-            <Card  />
 
-        </div>
 
 
     <Footer/>
